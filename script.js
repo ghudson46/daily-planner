dcd72d7c1273fe8085e67ad44c6a8408bd949347
Garrett Hudson <garretthudson87@yahoo.com>
2020-07-24 00:13:13 -0400

    created an interval that updates the time every 15 seconds

--- script.js
+++ script.js
@@ -10,42 +10,47 @@ $(document).ready(function() {
     console.log('time:', time);
 
     // save the value in localStorage as time
-    
+    localStorage.setItem(value, time); //stores the value and time in the local storage
   });
 
   function hourUpdater() {
     // get current number of hours
-    var currentHour = moment().hours();
+    var currentHour = moment().hours(); //gets the current hour by grabbing the current moment (mount day year hours minutes seconds) and just grabbing the hour
     console.log('current hour:', currentHour);
 
     // loop over time blocks
-    $(".time-block").each(function() {
+    $(".time-block").each(function() { //runs the function for each time block 9am - 5pm
       var blockHour = parseInt($(this).attr("id").split("-")[1]);
+      var textArea = $("textarea");
+      
+
 
       console.log("block hour:", blockHour);
 
       // check if we've moved past this time
-      
-      // if the current hour is greater than the block hour
-      // then add class "past"
+      if (currentHour > blockHour) {
+        $(".time-block").addClass('past'); //if current time is greater than the time in the block, the text area turns gray
+      } else if (currentHour === blockHour) {
+        $(".time-block").addClass('present'); //if current time is the same as the time in the block, the text area turns red
+        } else {
+          $(".time-block").addClass('future'); //if the current time is less than the block, the text turns red
+        };
+      })
+    };
+    hourUpdater();
+  })
 
-      // if they are equal
-      // then remove class "past" and add class "present"
-
-      // else
-      // remove class "past", remove class "present", add class "future"
-      
-    });
-  }
-
-  hourUpdater();
+  
 
   // set up interval to check if current time needs to be updated
   // which means execute hourUpdater function every 15 seconds
+  setInterval(function() {
+    hourUpdater();
+  }, 1500
+  );
 
   // load any saved data from localStorage
   
-
   // display current day on page
   $("#currentDay").text(moment().format("dddd, MMMM Do"));
-});
+
